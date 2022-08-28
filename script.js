48e6b214dd8c145c9097bd4ed89286467ea2a9e1
nicu <ontanicolae15@gmail.com>
2022-08-28 13:58:41 +0300

    Wired  up the js to make it functional

--- script.js
+++ script.js
@@ -17,16 +17,55 @@ function divide(a, b) {
 function operate(operator, a, b) {
     switch (operator) {
         case '+':
-            add(a, b);
-            break;
+            return add(a, b);
         case '-':
-            subtract(a, b);
-            break;
+            return subtract(a, b);
         case '*':
-            multiply(a, b);
-            break;
+            return multiply(a, b);
         case '/':
-            divide(a, b);
-            break;
+            return divide(a, b);
     }
 }
+
+const result = document.querySelector('.result');
+const operation = document.querySelector('.operation');
+
+let firstNumber, secondNumber;
+
+const numberButtons = document.querySelectorAll('.number');
+
+for (let i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener('click', () => {
+        if (operation.textContent === '') {
+            result.textContent = '';
+        }
+        result.textContent += numberButtons[i].textContent;
+    });
+}
+
+const operators = document.querySelectorAll('.operator');
+
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click', () => {
+        firstNumber = parseInt(result.textContent);
+        operation.textContent = operators[i].textContent;
+        result.textContent = '';
+    });
+}
+
+const equals = document.querySelector('.equals');
+
+equals.addEventListener('click', () => {
+    secondNumber = parseInt(result.textContent);
+    result.textContent = operate(operation.textContent, firstNumber, secondNumber);
+    operation.textContent = '';
+})
+
+const clear = document.querySelector('.clear');
+
+clear.addEventListener('click', () => {
+    firstNumber = undefined;
+    secondNumber = undefined;
+    result.textContent = '';
+    operation.textContent = '';
+})

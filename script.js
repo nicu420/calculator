9f5e2babf9b369c767f94f64639e5ae5fde62470
nicu <ontanicolae15@gmail.com>
2022-08-28 16:17:09 +0300

    Made it work

--- script.js
+++ script.js
@@ -28,18 +28,21 @@ function operate(operator, a, b) {
 }
 
 const result = document.querySelector('.result');
+result.textContent = 0;
 const operation = document.querySelector('.operation');
 
-let firstNumber, secondNumber;
+let firstNumber = 0, secondNumber, operator = '', value;
 
 const numberButtons = document.querySelectorAll('.number');
 
 for (let i = 0; i < numberButtons.length; i++) {
     numberButtons[i].addEventListener('click', () => {
-        if (operation.textContent === '') {
-            result.textContent = '';
+        result.textContent = parseInt(result.textContent + numberButtons[i].textContent);
+        if (operator === '') {
+            operation.textContent = '';
+        } else {
+            operation.textContent += numberButtons[i].textContent;
         }
-        result.textContent += numberButtons[i].textContent;
     });
 }
 
@@ -47,8 +50,11 @@ const operators = document.querySelectorAll('.operator');
 
 for (let i = 0; i < operators.length; i++) {
     operators[i].addEventListener('click', () => {
-        firstNumber = parseInt(result.textContent);
-        operation.textContent = operators[i].textContent;
+        if (operator === '') {
+            firstNumber = parseInt(result.textContent);
+        }
+        operator = operators[i].textContent;
+        operation.textContent = parseInt(firstNumber) + operator;
         result.textContent = '';
     });
 }
@@ -57,15 +63,17 @@ const equals = document.querySelector('.equals');
 
 equals.addEventListener('click', () => {
     secondNumber = parseInt(result.textContent);
-    result.textContent = operate(operation.textContent, firstNumber, secondNumber);
-    operation.textContent = '';
+    result.textContent = operate(operator, firstNumber, secondNumber);
+    operation.textContent = parseInt(firstNumber) + operator + parseInt(secondNumber) + ' = ';
+    operator = '';
 })
 
 const clear = document.querySelector('.clear');
 
 clear.addEventListener('click', () => {
-    firstNumber = undefined;
+    firstNumber = 0;
     secondNumber = undefined;
-    result.textContent = '';
+    result.textContent = 0;
     operation.textContent = '';
+    operator = '';
 })
